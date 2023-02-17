df3ea4ea9a9e872a4f20c50c87b4f347deeb5601
CalrethonOfMirkwood <821098sophiep11037@gmail.com>
2023-02-17 09:23:24 -0800

    cors

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -21,10 +21,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
     }
 
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/person/post").allowedOrigins("*");
-    }
-
+    /*
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/person/post").allowedOrigins("*");
+        }
+    */
 }
