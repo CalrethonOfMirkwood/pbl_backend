43d3453e292b9164cd3032d85563e31e9593e826
CalrethonOfMirkwood <82109882+CalrethonOfMirkwood@users.noreply.github.com>
2023-02-23 10:12:58 -0800

    allow signup

--- src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.antMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
 				.antMatchers("/api/person/**").authenticated()
+				.antMatchers("/api/person/post").permitAll()
 				.anyRequest().permitAll()
 				.and()
 			// support cors
