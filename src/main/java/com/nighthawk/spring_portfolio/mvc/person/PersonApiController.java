992e7d371e94d980e47662fde8b1074fbcf43d81
CalrethonOfMirkwood <82109882+CalrethonOfMirkwood@users.noreply.github.com>
2023-02-27 01:09:10 -0800

    okay it should work now

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -18,7 +19,9 @@ public class PersonApiController {
     #### RESTful API ####
     Resource: https://spring.io/guides/gs/rest-service/
     */
-
+    @Autowired  // Inject PasswordEncoder
+    private PasswordEncoder passwordEncoder;
+    
     // Autowired enables Control to connect POJO Object through JPA
     @Autowired
     private PersonJpaRepository repository;
@@ -80,7 +83,7 @@ public class PersonApiController {
         Person person = new Person();
         person.setName(name);
         person.setEmail(email);
-        person.setPassword(password);
+        person.setPassword(passwordEncoder.encode(password));
         person.setDob(dob);
         System.out.println("22222222 checkpoint 22222222");
         repository.save(person);
